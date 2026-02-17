fd727ff33d1deec50b0139f0c24822b9a5bfe60b
Jason1999b <jason.bisiaux@oteria.fr>
2026-02-17 16:16:09 +0100

    my_atoi genepy

---
+++ my_atol.c
@@ -0,0 +1,25 @@
+int my_atoi(const char *s) {
+    
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+    
+    if (s[0] == '-') {
+        sign = -1;
+        i++;
+    }
+    else if (s[0] == '+') {
+        i++;
+    }
+    
+  while (s[i] != '\0') {
+        if (s[i] < '0' || s[i] > '9') {
+            break;
+        }
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return result * sign;
+
+}
