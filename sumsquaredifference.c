5ca9fd7eaba43f8a627850be56e6f260023cffcc
Jason1999b <jason.bisiaux@oteria.fr>
2026-03-10 15:39:39 +0100

    ProjectEuler, SumSquareDifference

---
+++ sumsquaredifference.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long sum = 0;
+    long sum_of_squares = 0;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        sum += i;
+        sum_of_squares += i * i;
+    }
+
+    long square_of_sum = sum * sum;
+    long difference = square_of_sum - sum_of_squares;
+
+    printf("%ld\n", difference);
+    return 0;
+}
