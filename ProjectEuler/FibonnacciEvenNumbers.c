56a3099459cb766c750b25fa56bc59ab0ed8e726
Jason1999b <jason.bisiaux@oteria.fr>
2026-03-16 16:47:01 +0100

    FibonnacciEvenNumbers ProjectEuler

---
+++ ProjectEuler/FibonnacciEvenNumbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main () {
+
+    int x, y, z, sum;
+    x = y = z = 1;
+    sum = 0;
+
+    for (z = 1; z <= 4000000; z = x + y) {
+        if (z % 2 == 0) {
+            sum += z;
+        }
+        x = y;
+        y = z;
+    }
+    printf("Sum of even Fibonacci numbers: %d\n", sum);
+
+}
