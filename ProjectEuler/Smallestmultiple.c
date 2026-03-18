4865b50f6445ac1282cdc25b6b566fef89600518
Jason1999b <jason.bisiaux@oteria.fr>
2026-03-18 18:55:59 +0100

    ProjectEuler Smallestmultiple

---
+++ ProjectEuler/Smallestmultiple.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    long long nombre = 20;
+    int trouve = 0;
+
+while (!trouve) {
+    trouve = 1;
+
+    for (int i = 1; i <= 20; i++) {
+        if (nombre % i != 0) {
+            trouve = 0;
+            break;
+        }
+    }
+
+    if (!trouve) {
+        nombre += 20;
+    }
+}
+
+printf("Résultat : %lld\n", nombre);
+return 0;
+
+}
